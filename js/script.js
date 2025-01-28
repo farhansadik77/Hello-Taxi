23c41e49ca79fab5e002d6a9c8dadd66b2e14ae2
Farhan Sadik <kazifarhan121@gmail.com>
2025-01-28 15:55:39 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// slick slider
+$(document).ready(function(){
+    $('.wrapper').slick({
+        dots: true,
+        infinite: false,
+        speed: 800,
+        slidesToShow: 2,
+        slidesToScroll: 1
+    });
+});
